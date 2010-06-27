074e5ed7b9029a3cb47966aa7c97100ee709a2ef
Andrew Gee <andrew@andrewgee.org>
2010-06-27 16:57:45 +0100

    Version bump to 0.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from DistUtilsExtra.command import *
 
 setup(name="gpxviewer",
-	version="0.1",
+	version="0.2.0",
 	author="Andrew Gee",
 	author_email="andrew@andrewgee.org",
 	maintainer="Andrew Gee",
