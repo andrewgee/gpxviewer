206716970cdfb93ed0f193f7fa27e75412453538
Andrew Gee <andrew@andrewgee.org>
2015-02-22 15:04:35 +0000

    Version 0.5.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from DistUtilsExtra.command import *
 
 setup(name="gpxviewer",
-	version="0.4.3",
+	version="0.5.0",
 	author="Andrew Gee",
 	author_email="andrew@andrewgee.org",
 	maintainer="Andrew Gee",
