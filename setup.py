dbb684b1170ea2e3124ac02f76fbed1ca86a15cf
Andrew Gee <andrew@andrewgee.net>
2020-03-25 21:16:15 +0000

    Version 1.1.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 from DistUtilsExtra.command import build_extra, build_i18n
 
 setup(name="gpxviewer",
-	version="1.0.1",
+	version="1.1.0",
 	author="Andrew Gee",
 	author_email="andrew@andrewgee.org",
 	maintainer="Andrew Gee",
